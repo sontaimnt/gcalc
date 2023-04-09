1d82d5d5a08cdbb02706f09d8d433d41a782953e
Rik <97382691+sontaimnt@users.noreply.github.com>
2023-04-09 17:09:33 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,20 +4,45 @@ import (
 	"fmt"
 )
 
-var operation string
+var version string = "0.1"
+var operation int
 var num1 int
-var num2 int 
+var num2 int
 
 func main() {
 	fmt.Println("Welcome to gcalc")
-	fmt.Println("[a] Addition")
-	fmt.Println("[s] Subtraction")
-	fmt.Println("[m] Multiplication")
-	fmt.Println("[d] Division")
+	fmt.Println("[1] Addition")
+	fmt.Println("[2] Subtraction")
+	fmt.Println("[3] Multiplication")
+	fmt.Println("[4] Division")
 	fmt.Print("Select your operation:- ")
-	fmt.Scanf("%d" , &operation)
-	switch operation{
-		case "a":
-			fmt.Print("Enter 1st number")
+	fmt.Scanf("%d", &operation)
+	switch operation {
+	case 1:
+		fmt.Print("Enter 1st number:- ")
+		fmt.Scanf("%d", &num1)
+		fmt.Print("Enter 2nd number:- ")
+		fmt.Scanf("%d", &num2)
+		fmt.Println("Answer = ", num1+num2)
+	case 2:
+		fmt.Print("Enter 1st number:- ")
+		fmt.Scanf("%d", &num1)
+		fmt.Print("Enter 2nd number:- ")
+		fmt.Scanf("%d", &num2)
+		fmt.Println("Answer = ", num1-num2)
+	case 3:
+		fmt.Print("Enter 1st number:- ")
+		fmt.Scanf("%d", &num1)
+		fmt.Print("Enter 2nd number:- ")
+		fmt.Scanf("%d", &num2)
+		fmt.Println("Answer = ", num1*num2)
+	case 4:
+		fmt.Print("Enter 1st number:- ")
+		fmt.Scanf("%d", &num1)
+		fmt.Print("Enter 2nd number:- ")
+		fmt.Scanf("%d", &num2)
+		fmt.Println("Answer = ", num1/num2)
+	default:
+		fmt.Println("Error: Invalid Operation")
 	}
 }
